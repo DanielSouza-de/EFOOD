162f9931c6b0ac9158d09262dd5763dbe655eb0e
DanielSouza-de <daniel.desouza2007@gmail.com>
2026-04-07 23:12:32 -0300

    teste

--- src/components/Checkout/index.tsx
+++ src/components/Checkout/index.tsx
@@ -141,11 +141,23 @@ const Checkout = ({ onBackToCart }: Props) => {
                     <S.Title>Pedido realizado - {data.orderId}</S.Title>
 
                     <p>
-                        Estamos felizes em informar que seu pedido já está em processo de preparação e, em breve, será entregue no endereço fornecido.
+                        Estamos felizes em informar que seu pedido já está em
+                        processo de preparação e, em breve, será entregue no
+                        endereço fornecido.
                     </p>
-                    <p>Gostaríamos de ressaltar que nossos entregadores não estão autorizados a realizar cobranças extras.</p>
-                    <p>Lembre-se da importância de higienizar as mãos após o recebimento do pedido, garantindo assim sua segurança e bem-estar durante a refeição.</p>
-                    <p>Esperamos que desfrute de uma deliciosa e agradável experiência gastronômica. Bom apetite!</p>
+                    <p>
+                        Gostaríamos de ressaltar que nossos entregadores não
+                        estão autorizados a realizar cobranças extras.
+                    </p>
+                    <p>
+                        Lembre-se da importância de higienizar as mãos após o
+                        recebimento do pedido, garantindo assim sua segurança e
+                        bem-estar durante a refeição.
+                    </p>
+                    <p>
+                        Esperamos que desfrute de uma deliciosa e agradável
+                        experiência gastronômica. Bom apetite!
+                    </p>
 
                     <Button type="button" onClick={onBackToCart}>
                         Concluir
